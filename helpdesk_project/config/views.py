ea8144877dd2d6d50fb8c714892f9f3eb538324a
jakrins777 <rayzera000@gmail.com>
2025-11-10 16:44:26 +0700

    edit views.py

--- helpdesk_project/config/views.py
+++ helpdesk_project/config/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+# ลบ 'from django.shortcuts import render'
+from django.http import HttpResponse # 👈 1. Import ตัวนี้แทน
 
 def homepage_view(request):
-    return render(request, 'index.html')
+    # 2. ให้มัน return ข้อความธรรมดาๆ แบบนี้
+    return HttpResponse("Test OK: Homepage view is working!")
