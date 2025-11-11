930a7bc9dc1bf4ed7c8f1c4b24fca70a2b6c02b1
jakrins777 <rayzera000@gmail.com>
2025-11-11 13:43:39 +0700

    edit urls.py and delete views.py

--- helpdesk_project/config/urls.py
+++ helpdesk_project/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- helpdesk_project/config/views.py
+++
@@ -1,6 +0,0 @@
-# ลบ 'from django.shortcuts import render'
-from django.http import HttpResponse # 👈 1. Import ตัวนี้แทน
-
-def homepage_view(request):
-    # 2. ให้มัน return ข้อความธรรมดาๆ แบบนี้
-    return HttpResponse("Test OK: Homepage view is working!")
